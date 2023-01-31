059fa0ac8b5d19e4242bcac84abf946e7c655eef
Buyaki <rittahbuyaki@gmail.com>
2023-01-31 23:53:54 +0300

    Add ul tag to add handbags to the list each time a handbag is added

--- scripts/main.js
+++ scripts/main.js
@@ -1,14 +1,36 @@
 const photoInput = document.querySelector('.handbag-photo');
 const handbagNameInput = document.querySelector('.handbag-name');
 const priceInput = document.querySelector('.price');
-const form = document.querySelector('form');
+const handbagForm = document.querySelector('form');
+const handbagList = document.querySelector('#handbag-list');
 
-form.addEventListener('submit', (event) => {
+handbagForm.addEventListener('submit', (event) => {
   event.preventDefault();
   const photo = photoInput.files[0];
   const handbagName = handbagNameInput.value;
   const price = priceInput.value;
-  console.log(`Handbag Photo: ${photo}`);
-  console.log(`Handbag Name: ${handbagName}`);
-  console.log(`Price: ${price}`);
+
+  const reader = new FileReader();
+  reader.readAsDataURL(photo);
+  reader.onload = function() {
+    const photoDataURL = reader.result;
+  
+    const handbagItem = document.createElement('li');
+    handbagItem.innerHTML = `
+      <div>
+        <div class="img-responsive">
+          <img src=${photoDataURL} alt="#">
+        </div>
+        <div class="text-center">
+          <h5>${handbagName}</h5>
+          <h6>${price}</h6>
+          <div>
+            <button>Edit</button>
+            <button>Delete</button>
+          </div>
+        </div>
+      </div>
+    `;
+    handbagList.appendChild(handbagItem);
+  };
 });
