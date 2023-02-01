392540f87c59cf49d28655fa8b240361f013fca9
Buyaki <rittahbuyaki@gmail.com>
2023-02-01 15:06:13 +0300

    Add functionality to make sure that data is preserved in the browser's memory by using localStorage

--- scripts/main.js
+++ scripts/main.js
@@ -26,4 +26,38 @@ form.addEventListener("submit", (event) => {
   });
   
   handbagList.appendChild(li);
+
+
+  const reader = new FileReader();
+  reader.onload = function() {
+    const dataURL = reader.result;
+    // Store the handbag data in the local storage
+    let handbags = JSON.parse(localStorage.getItem("handbags")) || [];
+    handbags.push({dataURL, handbagName, price});
+    localStorage.setItem("handbags", JSON.stringify(handbags));
+  };
+  reader.readAsDataURL(handbagPhoto);
 });
+// Retrieve the stored data and recreate the list items on page reload
+window.addEventListener("load", () => {
+  const handbags = JSON.parse(localStorage.getItem("handbags")) || [];
+  handbags.forEach((handbag) => {
+    const li = document.createElement("li");
+    li.innerHTML = `
+      <div> <img class="img-responsive" src="${handbag.dataURL}"/> </div>
+      <div> <span>${handbag.handbagName}</span> </div>
+      <div> <span>${handbag.price}</span>  </div>
+      <div>
+        <button class="edit">Edit</button>
+        <button class="delete">Delete</button>
+      </div>
+    `;
+
+    li.querySelector(".delete").addEventListener("click", (event) => {
+      li.remove();
+    });
+
+    handbagList.appendChild(li);
+  });
+});
+
